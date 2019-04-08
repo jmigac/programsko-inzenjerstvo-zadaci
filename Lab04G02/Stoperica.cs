7cfcccc0ec739bb42c1df8b50c08a7dbca75814a
Jurica Migač <jmigac@foi.hr>
2019-04-08 15:51:27 +0200

    Dodane funkcionalnosti za upravljanjem vremenom kruga.

---
+++ Lab04G02/Stoperica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04G02
+{
+    class Stoperica
+    {
+        public static int MiliSekunda { get; set; }
+        public static int Sekunda { get; set; }
+        public static int Minuta { get; set; }
+        public static bool AktivnaStoperica;
+       
+    }
+}
