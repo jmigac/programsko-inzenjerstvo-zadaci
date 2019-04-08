4a98fe97184f943630923eeaf5400f76fee38d90
Jurica Migač <jmigac@foi.hr>
2019-04-08 21:15:11 +0200

    Aplikacija za prikaz otplanog plana kroz zadane vremenske intervale.

--- Zadatak137/Form1.Designer.cs
+++ Zadatak137/Form1.Designer.cs
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtGlavnica = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtKamatnaStopa = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtTrajanjeRacuna = new System.Windows.Forms.TextBox();
+            this.BtnOtplatniPlan = new System.Windows.Forms.Button();
+            this.ListaIspisOtplate = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Glavnica:";
+            // 
+            // TxtGlavnica
+            // 
+            this.TxtGlavnica.Location = new System.Drawing.Point(95, 12);
+            this.TxtGlavnica.Name = "TxtGlavnica";
+            this.TxtGlavnica.Size = new System.Drawing.Size(198, 20);
+            this.TxtGlavnica.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kamatna stopa:";
+            // 
+            // TxtKamatnaStopa
+            // 
+            this.TxtKamatnaStopa.Location = new System.Drawing.Point(95, 38);
+            this.TxtKamatnaStopa.Name = "TxtKamatnaStopa";
+            this.TxtKamatnaStopa.Size = new System.Drawing.Size(198, 20);
+            this.TxtKamatnaStopa.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Trajanje računa:";
+            // 
+            // TxtTrajanjeRacuna
+            // 
+            this.TxtTrajanjeRacuna.Location = new System.Drawing.Point(95, 64);
+            this.TxtTrajanjeRacuna.Name = "TxtTrajanjeRacuna";
+            this.TxtTrajanjeRacuna.Size = new System.Drawing.Size(198, 20);
+            this.TxtTrajanjeRacuna.TabIndex = 3;
+            // 
+            // BtnOtplatniPlan
+            // 
+            this.BtnOtplatniPlan.Location = new System.Drawing.Point(15, 96);
+            this.BtnOtplatniPlan.Name = "BtnOtplatniPlan";
+            this.BtnOtplatniPlan.Size = new System.Drawing.Size(278, 23);
+            this.BtnOtplatniPlan.TabIndex = 4;
+            this.BtnOtplatniPlan.Text = "Otplani plan";
+            this.BtnOtplatniPlan.UseVisualStyleBackColor = true;
+            this.BtnOtplatniPlan.Click += new System.EventHandler(this.BtnOtplatniPlan_Click);
+            // 
+            // ListaIspisOtplate
+            // 
+            this.ListaIspisOtplate.FormattingEnabled = true;
+            this.ListaIspisOtplate.Location = new System.Drawing.Point(15, 126);
+            this.ListaIspisOtplate.Name = "ListaIspisOtplate";
+            this.ListaIspisOtplate.Size = new System.Drawing.Size(278, 303);
+            this.ListaIspisOtplate.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(305, 450);
+            this.Controls.Add(this.ListaIspisOtplate);
+            this.Controls.Add(this.BtnOtplatniPlan);
+            this.Controls.Add(this.TxtTrajanjeRacuna);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtKamatnaStopa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtGlavnica);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Otplatni plan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtGlavnica;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtKamatnaStopa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtTrajanjeRacuna;
+        private System.Windows.Forms.Button BtnOtplatniPlan;
+        private System.Windows.Forms.ListBox ListaIspisOtplate;
     }
 }
 
--- Zadatak137/Form1.cs
+++ Zadatak137/Form1.cs
@@ -16,5 +16,29 @@ namespace Zadatak137
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnOtplatniPlan_Click(object sender, EventArgs e)
+        {
+            ListaIspisOtplate.Items.Clear();
+            double glavnica = double.Parse(TxtGlavnica.Text);
+            double kamata = double.Parse(TxtKamatnaStopa.Text);
+            int trajanjeKredita = int.Parse(TxtTrajanjeRacuna.Text);
+            double kredit = glavnica + glavnica * kamata;
+            double rataKredita = kredit / double.Parse(trajanjeKredita.ToString());
+            for (int i = 0; i < trajanjeKredita; i++)
+            {
+                ListaIspisOtplate.Items.Add(String.Format($"{i + 1}.mjesec - {kredit:0.00} - {rataKredita:0.00}HRK"));
+                kredit -= rataKredita;
+            }
+            ListaIspisOtplate.Items.Add(String.Format($"Otplaćen kredit!"));
+            TxtGlavnica.Text = "";
+            TxtKamatnaStopa.Text = "";
+            TxtTrajanjeRacuna.Text = "";
+        }
     }
 }
