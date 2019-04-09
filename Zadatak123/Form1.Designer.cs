0b952ff346cde87c37f0f9bd4747f08b84bf12af
Jurica Migač <jmigac@foi.hr>
2019-04-09 17:50:40 +0200

    Aplikacija koja omogućava pristup webstranici s unesenim linkom korisnika i ujedno bilježi povijest pretraživanja i vrijeme u milisekundama za pristup pojedinoj web stranici.

--- Zadatak123/Form1.Designer.cs
+++ Zadatak123/Form1.Designer.cs
@@ -29,12 +29,91 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.WebBrowser = new System.Windows.Forms.WebBrowser();
+            this.ListaPovijestPretrazivanja = new System.Windows.Forms.ListBox();
+            this.TxtLink = new System.Windows.Forms.TextBox();
+            this.BtnPretrazi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.VrijemeUcitavanja = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // WebBrowser
+            // 
+            this.WebBrowser.Location = new System.Drawing.Point(1, 35);
+            this.WebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowser.Name = "WebBrowser";
+            this.WebBrowser.Size = new System.Drawing.Size(859, 493);
+            this.WebBrowser.TabIndex = 0;
+            this.WebBrowser.Url = new System.Uri("", System.UriKind.Relative);
+            this.WebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser_DocumentCompleted);
+            // 
+            // ListaPovijestPretrazivanja
+            // 
+            this.ListaPovijestPretrazivanja.FormattingEnabled = true;
+            this.ListaPovijestPretrazivanja.Location = new System.Drawing.Point(867, 35);
+            this.ListaPovijestPretrazivanja.Name = "ListaPovijestPretrazivanja";
+            this.ListaPovijestPretrazivanja.Size = new System.Drawing.Size(280, 498);
+            this.ListaPovijestPretrazivanja.TabIndex = 1;
+            // 
+            // TxtLink
+            // 
+            this.TxtLink.Location = new System.Drawing.Point(50, 12);
+            this.TxtLink.Name = "TxtLink";
+            this.TxtLink.Size = new System.Drawing.Size(810, 20);
+            this.TxtLink.TabIndex = 2;
+            // 
+            // BtnPretrazi
+            // 
+            this.BtnPretrazi.Location = new System.Drawing.Point(867, 9);
+            this.BtnPretrazi.Name = "BtnPretrazi";
+            this.BtnPretrazi.Size = new System.Drawing.Size(280, 23);
+            this.BtnPretrazi.TabIndex = 3;
+            this.BtnPretrazi.Text = "Pretraži";
+            this.BtnPretrazi.UseVisualStyleBackColor = true;
+            this.BtnPretrazi.Click += new System.EventHandler(this.BtnPretrazi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // VrijemeUcitavanja
+            // 
+            this.VrijemeUcitavanja.Interval = 10;
+            this.VrijemeUcitavanja.Tick += new System.EventHandler(this.VrijemeUcitavanja_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1147, 531);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnPretrazi);
+            this.Controls.Add(this.TxtLink);
+            this.Controls.Add(this.ListaPovijestPretrazivanja);
+            this.Controls.Add(this.WebBrowser);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Web Preglednik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser WebBrowser;
+        private System.Windows.Forms.ListBox ListaPovijestPretrazivanja;
+        private System.Windows.Forms.TextBox TxtLink;
+        private System.Windows.Forms.Button BtnPretrazi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer VrijemeUcitavanja;
     }
 }
 
--- Zadatak123/Form1.cs
+++ Zadatak123/Form1.cs
@@ -12,9 +12,67 @@ namespace Zadatak123
 {
     public partial class Form1 : Form
     {
+        public static int Minuta;
+        public static int Sekunda;
+        public static int MiliSekunda;
+        public static bool AktivnoBrojanjeVremena;
+        public static int PotrebnoVrijemeUcitavanja;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnPretrazi_Click(object sender, EventArgs e)
+        {
+            if(TxtLink.Text.Length>0 && TxtLink.Text.Contains("http://www"))
+            {
+                Uri noviLink = new Uri(TxtLink.Text);
+                WebBrowser.Url = noviLink;
+                VrijemeUcitavanja.Enabled = true;
+                AktivnoBrojanjeVremena = true;
+            }
+        }
+
+        private void VrijemeUcitavanja_Tick(object sender, EventArgs e)
+        {
+            if (AktivnoBrojanjeVremena)
+            {
+                MiliSekunda++;
+                if (MiliSekunda >= 100)
+                {
+                    Sekunda++;
+                    MiliSekunda = 0;
+                }
+                if (Sekunda >= 59)
+                {
+                    Minuta++;
+                    Sekunda = 0;
+                }
+            }
+        }
+        private void ResetirajVrijeme()
+        {
+            Minuta = 0;
+            Sekunda = 0;
+            MiliSekunda = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetirajVrijeme();
+            AktivnoBrojanjeVremena = false;
+            
+        }
+
+        private void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            AktivnoBrojanjeVremena = false;
+            VrijemeUcitavanja.Enabled = false;
+            PotrebnoVrijemeUcitavanja = MiliSekunda + (Sekunda * 1000) + (Minuta * 60 * 1000);
+            string upisPovijest = "";
+            upisPovijest += TxtLink.Text + " - " + PotrebnoVrijemeUcitavanja + "ms";
+            if(WebBrowser.Url.ToString().Length>0)
+                ListaPovijestPretrazivanja.Items.Add(upisPovijest);
+            ResetirajVrijeme();
         }
     }
 }
