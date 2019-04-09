19fddddeedf4ea7ad15023dbce8291420fd69c96
Jurica Migač <jmigac@foi.hr>
2019-04-09 14:06:01 +0200

    Aplikacija za pretvorbu binarnog u dekatski broj.

--- Zadatak133/Form1.Designer.cs
+++ Zadatak133/Form1.Designer.cs
@@ -28,13 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtPrviBroj = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtDrugiBroj = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtTreciBroj = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtCetvrtiBroj = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtPetiBroj = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtSestBroj = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtSedmiBroj = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TxtOsmiBroj = new System.Windows.Forms.TextBox();
+            this.LabelaDekatski = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1 znamenka:";
+            // 
+            // TxtPrviBroj
+            // 
+            this.TxtPrviBroj.Location = new System.Drawing.Point(88, 13);
+            this.TxtPrviBroj.MaxLength = 1;
+            this.TxtPrviBroj.Name = "TxtPrviBroj";
+            this.TxtPrviBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtPrviBroj.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2 znamenka:";
+            // 
+            // TxtDrugiBroj
+            // 
+            this.TxtDrugiBroj.Location = new System.Drawing.Point(88, 39);
+            this.TxtDrugiBroj.MaxLength = 1;
+            this.TxtDrugiBroj.Name = "TxtDrugiBroj";
+            this.TxtDrugiBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtDrugiBroj.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "3 znamenka:";
+            // 
+            // TxtTreciBroj
+            // 
+            this.TxtTreciBroj.Location = new System.Drawing.Point(88, 65);
+            this.TxtTreciBroj.MaxLength = 1;
+            this.TxtTreciBroj.Name = "TxtTreciBroj";
+            this.TxtTreciBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtTreciBroj.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "4 znamenka:";
+            // 
+            // TxtCetvrtiBroj
+            // 
+            this.TxtCetvrtiBroj.Location = new System.Drawing.Point(88, 91);
+            this.TxtCetvrtiBroj.MaxLength = 1;
+            this.TxtCetvrtiBroj.Name = "TxtCetvrtiBroj";
+            this.TxtCetvrtiBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtCetvrtiBroj.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "5 znamenka:";
+            // 
+            // TxtPetiBroj
+            // 
+            this.TxtPetiBroj.Location = new System.Drawing.Point(88, 117);
+            this.TxtPetiBroj.MaxLength = 1;
+            this.TxtPetiBroj.Name = "TxtPetiBroj";
+            this.TxtPetiBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtPetiBroj.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "6 znamenka:";
+            // 
+            // TxtSestBroj
+            // 
+            this.TxtSestBroj.Location = new System.Drawing.Point(88, 143);
+            this.TxtSestBroj.MaxLength = 1;
+            this.TxtSestBroj.Name = "TxtSestBroj";
+            this.TxtSestBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtSestBroj.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "7 znamenka:";
+            // 
+            // TxtSedmiBroj
+            // 
+            this.TxtSedmiBroj.Location = new System.Drawing.Point(88, 169);
+            this.TxtSedmiBroj.MaxLength = 1;
+            this.TxtSedmiBroj.Name = "TxtSedmiBroj";
+            this.TxtSedmiBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtSedmiBroj.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "8 znamenka:";
+            // 
+            // TxtOsmiBroj
+            // 
+            this.TxtOsmiBroj.Location = new System.Drawing.Point(88, 195);
+            this.TxtOsmiBroj.MaxLength = 1;
+            this.TxtOsmiBroj.Name = "TxtOsmiBroj";
+            this.TxtOsmiBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtOsmiBroj.TabIndex = 8;
+            // 
+            // LabelaDekatski
+            // 
+            this.LabelaDekatski.AutoSize = true;
+            this.LabelaDekatski.Location = new System.Drawing.Point(85, 257);
+            this.LabelaDekatski.Name = "LabelaDekatski";
+            this.LabelaDekatski.Size = new System.Drawing.Size(35, 13);
+            this.LabelaDekatski.TabIndex = 2;
+            this.LabelaDekatski.Text = "label9";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Prikazi decimalno";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(210, 277);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LabelaDekatski);
+            this.Controls.Add(this.TxtOsmiBroj);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.TxtSedmiBroj);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TxtSestBroj);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtPetiBroj);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtCetvrtiBroj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtTreciBroj);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtDrugiBroj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtPrviBroj);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Prikaz binarnog u dekatsko";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtPrviBroj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtDrugiBroj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtTreciBroj;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtCetvrtiBroj;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtPetiBroj;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtSestBroj;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TxtSedmiBroj;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TxtOsmiBroj;
+        private System.Windows.Forms.Label LabelaDekatski;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Zadatak133/Form1.cs
+++ Zadatak133/Form1.cs
@@ -16,5 +16,24 @@ namespace Zadatak133
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TxtPrviBroj.Text == "0" || TxtPrviBroj.Text=="1" 
+                && TxtDrugiBroj.Text == "0" || TxtDrugiBroj.Text == "1"
+                && TxtTreciBroj.Text == "0" || TxtTreciBroj.Text == "1" )
+            {
+                string binarniBroj = "";
+                binarniBroj += TxtPrviBroj.Text + TxtDrugiBroj.Text + TxtTreciBroj.Text +
+                               TxtCetvrtiBroj.Text + TxtPetiBroj.Text + TxtSestBroj.Text +
+                               TxtSedmiBroj.Text + TxtOsmiBroj.Text;
+                LabelaDekatski.Text = Convert.ToInt32(binarniBroj,2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Unijeli ste ne binarnu znamenku!");
+            }
+
+        }
     }
 }
