67f9871d19541596a829ec44ca612182c1ef356b
Jurica Migač <jmigac@foi.hr>
2019-04-09 15:48:57 +0200

    Aplikacija za provjeru palindroma, najčešćeg slova i broja ponavljanja toga slova.

--- Zadatak-110/Form1.Designer.cs
+++ Zadatak-110/Form1.Designer.cs
@@ -29,12 +29,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtUneseniTekst = new System.Windows.Forms.TextBox();
+            this.ListaStatistike = new System.Windows.Forms.ListBox();
+            this.TimerPromjene = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Uneseni tekst:";
+            // 
+            // TxtUneseniTekst
+            // 
+            this.TxtUneseniTekst.Location = new System.Drawing.Point(13, 30);
+            this.TxtUneseniTekst.Name = "TxtUneseniTekst";
+            this.TxtUneseniTekst.Size = new System.Drawing.Size(145, 20);
+            this.TxtUneseniTekst.TabIndex = 1;
+            this.TxtUneseniTekst.TextChanged += new System.EventHandler(this.TxtUneseniTekst_TextChanged);
+            // 
+            // ListaStatistike
+            // 
+            this.ListaStatistike.FormattingEnabled = true;
+            this.ListaStatistike.Location = new System.Drawing.Point(13, 61);
+            this.ListaStatistike.Name = "ListaStatistike";
+            this.ListaStatistike.Size = new System.Drawing.Size(145, 381);
+            this.ListaStatistike.TabIndex = 3;
+            // 
+            // TimerPromjene
+            // 
+            this.TimerPromjene.Enabled = true;
+            this.TimerPromjene.Tick += new System.EventHandler(this.TimerPromjene_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(170, 450);
+            this.Controls.Add(this.ListaStatistike);
+            this.Controls.Add(this.TxtUneseniTekst);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtUneseniTekst;
+        private System.Windows.Forms.ListBox ListaStatistike;
+        private System.Windows.Forms.Timer TimerPromjene;
     }
 }
 
--- Zadatak-110/Form1.cs
+++ Zadatak-110/Form1.cs
@@ -16,5 +16,83 @@ namespace Zadatak_110
         {
             InitializeComponent();
         }
+        private void ProvjeraPalindroma(string rijec)
+        {
+            for (int i = 0; i < rijec.Length/2; i++)
+            {
+                if (rijec[i].Equals(rijec[rijec.Length - 1 - i]))
+                {
+                    TimerPromjene.Enabled = false;
+                    MessageBox.Show("Palindrom");
+                    break;
+                }
+            }
+        }
+        private bool ProvjeraSlovaUListi(List<char> slova, char slovo)
+        {
+            foreach (var item in slova)
+            {
+                if (item.Equals(slovo))
+                    return true;
+            }
+            return false;
+        }
+        private int NadiSlovo(int[] polje, int vrijednost)
+        {
+            for (int i = 0; i < polje.Length; i++)
+            {
+                if (polje[i].Equals(vrijednost))
+                    return i;
+            }
+            return 0;
+        }
+        private void StatistikaRijeci(string rijec)
+        {
+            if (rijec.Length > 0)
+            {
+                string brojSlova = rijec.Length.ToString();
+                List<char> slova = new List<char>();
+                for (int i = 0; i < rijec.Length; i++)
+                {
+                    if (!ProvjeraSlovaUListi(slova, rijec[i]))
+                    {
+                        slova.Add(rijec[i]);
+                    }
+                }
+                int[] brojPonavljanjaSlova = new int[slova.Count];
+                for (int i = 0; i < slova.Count; i++)
+                {
+                    brojPonavljanjaSlova[i] = 0;
+                }
+                for (int i = 0; i < slova.Count; i++)
+                {
+                    for (int j = 0; j < rijec.Length; j++)
+                    {
+                        if (slova[i].Equals(rijec[j]))
+                            brojPonavljanjaSlova[i]++;
+                    }
+                }
+                int najcesceSlovo, najrjedeSlovo;
+                Array.Sort(brojPonavljanjaSlova);
+                najcesceSlovo = brojPonavljanjaSlova[brojPonavljanjaSlova.Length-1];
+                najrjedeSlovo = brojPonavljanjaSlova[0];
+                int najcesceSlovoIndex = NadiSlovo(brojPonavljanjaSlova, najcesceSlovo);
+                int najrjedeSlovoIndex = NadiSlovo(brojPonavljanjaSlova, najrjedeSlovo);
+                string ispis = String.Format($"Najcesce:{slova[najcesceSlovoIndex]}-{najcesceSlovo} | Najrjede:{slova[najrjedeSlovoIndex]}-{najrjedeSlovo}");
+                ListaStatistike.Items.Add(ispis);
+                ispis = "";
+                TimerPromjene.Enabled = false;
+            }
+        }
+        private void TimerPromjene_Tick(object sender, EventArgs e)
+        {
+            ProvjeraPalindroma(TxtUneseniTekst.Text);
+            StatistikaRijeci(TxtUneseniTekst.Text);
+        }
+
+        private void TxtUneseniTekst_TextChanged(object sender, EventArgs e)
+        {
+            TimerPromjene.Enabled = true;
+        }
     }
 }
