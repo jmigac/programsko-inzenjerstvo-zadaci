149061b87a0032d62df946c2b504a8a566fdfbdf
Jurica Migač <jmigac@foi.hr>
2019-04-09 14:41:39 +0200

    Aplikacija za računanje brojeva dijelivih s X u rasponu od a do b.

--- Zadatak140/Form1.Designer.cs
+++ Zadatak140/Form1.Designer.cs
@@ -29,12 +29,110 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtPrviBroj = new System.Windows.Forms.TextBox();
+            this.TxtDrugiBroj = new System.Windows.Forms.TextBox();
+            this.TxtTreciBroj = new System.Windows.Forms.TextBox();
+            this.ListaBrojeva = new System.Windows.Forms.ListBox();
+            this.TimerProvjere = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prvi broj:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Drugi broj:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Treći broj:";
+            // 
+            // TxtPrviBroj
+            // 
+            this.TxtPrviBroj.Location = new System.Drawing.Point(68, 10);
+            this.TxtPrviBroj.Name = "TxtPrviBroj";
+            this.TxtPrviBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtPrviBroj.TabIndex = 1;
+            this.TxtPrviBroj.TextChanged += new System.EventHandler(this.TxtPrviBroj_TextChanged);
+            // 
+            // TxtDrugiBroj
+            // 
+            this.TxtDrugiBroj.Location = new System.Drawing.Point(68, 37);
+            this.TxtDrugiBroj.Name = "TxtDrugiBroj";
+            this.TxtDrugiBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtDrugiBroj.TabIndex = 2;
+            this.TxtDrugiBroj.TextChanged += new System.EventHandler(this.TxtDrugiBroj_TextChanged);
+            // 
+            // TxtTreciBroj
+            // 
+            this.TxtTreciBroj.Location = new System.Drawing.Point(68, 63);
+            this.TxtTreciBroj.Name = "TxtTreciBroj";
+            this.TxtTreciBroj.Size = new System.Drawing.Size(100, 20);
+            this.TxtTreciBroj.TabIndex = 3;
+            this.TxtTreciBroj.TextChanged += new System.EventHandler(this.TxtTreciBroj_TextChanged);
+            // 
+            // ListaBrojeva
+            // 
+            this.ListaBrojeva.FormattingEnabled = true;
+            this.ListaBrojeva.Location = new System.Drawing.Point(13, 92);
+            this.ListaBrojeva.Name = "ListaBrojeva";
+            this.ListaBrojeva.Size = new System.Drawing.Size(155, 121);
+            this.ListaBrojeva.TabIndex = 4;
+            // 
+            // TimerProvjere
+            // 
+            this.TimerProvjere.Enabled = true;
+            this.TimerProvjere.Interval = 2500;
+            this.TimerProvjere.Tick += new System.EventHandler(this.TimerProvjere_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(180, 219);
+            this.Controls.Add(this.ListaBrojeva);
+            this.Controls.Add(this.TxtTreciBroj);
+            this.Controls.Add(this.TxtDrugiBroj);
+            this.Controls.Add(this.TxtPrviBroj);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtPrviBroj;
+        private System.Windows.Forms.TextBox TxtDrugiBroj;
+        private System.Windows.Forms.TextBox TxtTreciBroj;
+        private System.Windows.Forms.ListBox ListaBrojeva;
+        private System.Windows.Forms.Timer TimerProvjere;
     }
 }
 
--- Zadatak140/Form1.cs
+++ Zadatak140/Form1.cs
@@ -16,5 +16,47 @@ namespace Zadatak140
         {
             InitializeComponent();
         }
+
+        private void TimerProvjere_Tick(object sender, EventArgs e)
+        {
+            int prviBroj, drugiBroj, treciBroj;
+            if (TxtPrviBroj.Text.Length>0 && TxtDrugiBroj.Text.Length > 0 && TxtTreciBroj.Text.Length > 0)
+            {
+                prviBroj = int.Parse(TxtPrviBroj.Text);
+                drugiBroj = int.Parse(TxtDrugiBroj.Text);
+                treciBroj = int.Parse(TxtTreciBroj.Text);
+                for (int i = prviBroj; i < drugiBroj; i++)
+                {
+                    if(i%treciBroj == 0)
+                    {
+                        ListaBrojeva.Items.Add(i);
+                    }
+                }
+                TimerProvjere.Enabled = false;
+            }
+            else
+            {
+                
+                ListaBrojeva.Items.Clear();
+            }
+            
+                
+
+        }
+
+        private void TxtPrviBroj_TextChanged(object sender, EventArgs e)
+        {
+            TimerProvjere.Enabled = true;
+        }
+
+        private void TxtDrugiBroj_TextChanged(object sender, EventArgs e)
+        {
+            TimerProvjere.Enabled = true;
+        }
+
+        private void TxtTreciBroj_TextChanged(object sender, EventArgs e)
+        {
+            TimerProvjere.Enabled = true;
+        }
     }
 }
